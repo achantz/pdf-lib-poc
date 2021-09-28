4853aa0fdb9b15c3b3f4bcb2d36e34c65effe7f5
Angelo Chantziantoniou <angelo.chantziantoniou@gov.ab.ca>
2021-09-28 16:16:11 -0600

    added file filter to check for XFA PDF files

--- apps/api/src/app/controllers/file/file.controller.ts
+++ apps/api/src/app/controllers/file/file.controller.ts
@@ -1,31 +1,38 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import {
-  BadRequestException,
-  Bind,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Res,
-  UploadedFile,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Bind, Controller, Delete, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express/multer';
 import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
 import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { Express } from 'express';
+import * as fs from 'fs';
 import { Multer } from 'multer';
 import { join } from 'path';
+import { PDFExtract } from 'pdf.js-extract';
 
 import { PDFInterceptor } from '../../interceptors/pdf.intrerceptor';
 import { CreateFileDto } from './dto/create-file.dto';
 import { FileService } from './file.service';
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
 export const multerOptions = {
   dest: './files',
+  fileFilter: (req, file, callback) => {
+    if (fs.existsSync(file.path) && file.mimetype === 'application/pdf') {
+      const extract = new PDFExtract();
+      const meta = extract.extract(file.path).then((meta) => {
+        if (meta) {
+          if (meta.meta?.info?.IsXFAPresent) {
+            return callback(
+              new Error('Cannot process PDF files with XFA forms'),
+              false
+            );
+          }
+        }
+      });
+    }
+    return callback(null, true);
+  },
 } as MulterOptions;
 
 // export const storage = {
